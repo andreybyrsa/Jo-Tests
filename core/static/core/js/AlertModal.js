1f4b67e502bb2964a4b32cf6c5173446383878e3
andreybyrsa <byrsa2004@bk.ru>
2023-07-06 14:04:19 +0500

    Сделал AlertModal

---
+++ core/static/core/js/AlertModal.js
@@ -0,0 +1,38 @@
+let alertModal = document.getElementById("alert-modal");
+
+let closeAlertModalButton = document.getElementById("close-alert-modal-button");
+
+const OPENING_ALERT_MODAL_CLASS = "alert-modal--opened";
+const CLOSING_ALERT_MODAL_CLASS = "alert-modal--closed";
+
+if (alertModal !== null) {
+  openAlertModal();
+}
+
+function openAlertModal() {
+  alertModal.classList.add(OPENING_ALERT_MODAL_CLASS);
+  openModal();
+}
+
+function closeAlertModal() {
+  alertModal.classList.add(CLOSING_ALERT_MODAL_CLASS);
+  setTimeout(() => {
+    alertModal.remove();
+    alertModal = null;
+  }, 300);
+}
+
+modalLayout.addEventListener("click", (event) => {
+  if (event.target.id === "modal-layout") {
+    if (alertModal) {
+      closeAlertModal();
+    }
+  }
+});
+
+if (closeAlertModalButton) {
+  closeAlertModalButton.addEventListener("click", () => {
+    closeAlertModal();
+    closeModal();
+  });
+}
