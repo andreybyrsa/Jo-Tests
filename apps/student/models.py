3aac2d331eb6c9374b6c4a0c7c64a5023be073f1
TimurMA <timur.minyazeff@gmail.com>
2023-07-06 14:56:53 +0300

    Добавил слаги

--- apps/student/models.py
+++ apps/student/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class Student(AbstractUser):
     profile_picture = models.FileField(null=True, blank=True, max_length=500)
     role = models.CharField(default='student')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.username
@@ -13,6 +14,7 @@ class StudentResult(models.Model):
     student = models.ForeignKey('Student', on_delete = models.CASCADE)
     test = models.ForeignKey('authorApp.Test', on_delete = models.CASCADE)
     result = models.FloatField()
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
 
 
@@ -21,3 +23,4 @@ class Choice(models.Model):
     student = models.ForeignKey('Student', on_delete = models.CASCADE)
     answer = models.ForeignKey('authorApp.Answer', on_delete = models.CASCADE)
     is_selected = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
