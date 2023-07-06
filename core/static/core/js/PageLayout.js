630917cf6f22d39997e29305089c9406b4473b74
andreybyrsa <byrsa2004@bk.ru>
2023-07-06 14:03:41 +0500

    Добавил js файл для страницы обертки, функция для page className, функция модального окна

---
+++ core/static/core/js/PageLayout.js
@@ -0,0 +1,50 @@
+let pageLayoutSideBar = document.querySelector(".page-layout__left-side-bar");
+let pageLayoutContentWrapper = document.querySelector(
+  ".page-layout__content-wrapper"
+);
+
+if (pageLayoutSideBar.children.length) {
+  pageLayoutContentWrapper.style.gap = "20px";
+}
+
+let modalLayout = document.getElementById("modal-layout");
+
+const OPENING_MODAL_LAYOUT_CLASS = "page-layout-modal--opened";
+const CLOSING_MODAL_LAYOUT_CLASS = "page-layout-modal--closed";
+
+function openModal() {
+  modalLayout.style.display = "grid";
+  modalLayout.classList.add(OPENING_MODAL_LAYOUT_CLASS);
+}
+
+function closeModal() {
+  modalLayout.classList.add(CLOSING_MODAL_LAYOUT_CLASS);
+  setTimeout(() => {
+    modalLayout.classList.remove(OPENING_MODAL_LAYOUT_CLASS);
+    modalLayout.classList.remove(CLOSING_MODAL_LAYOUT_CLASS);
+    modalLayout.style.display = "none";
+  }, 300);
+}
+
+modalLayout.addEventListener("click", (event) => {
+  if (event.target.id === "modal-layout") {
+    closeModal();
+  }
+});
+
+function addPageClassName(className) {
+  let pageLayoutElements = Array.from(
+    document.querySelectorAll("[class^=page-layout]")
+  ).slice(1);
+
+  pageLayoutElements.forEach((element) => {
+    let currentClassName = element.className;
+    let childClassName = element.className.split("__")[1];
+
+    if (childClassName) {
+      element.className = `${className}__${childClassName} ${currentClassName}`;
+    } else {
+      element.className = `${className} ${currentClassName}`;
+    }
+  });
+}
