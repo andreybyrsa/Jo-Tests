5981111e4e204ed4975dca3ec8612548b51eddcc
TimurMA <timur.minyazeff@gmail.com>
2023-07-06 13:37:26 +0300

    Добавил модели группы и курса

--- apps/teacher/models.py
+++ apps/teacher/models.py
@@ -6,3 +6,26 @@ class Teacher(AbstractUser):
     role = models.CharField(default='teacher')
     courses = models.ManyToManyField('Course')
     groups = models.ManyToManyField('Group')
+
+class Group(models.Model):
+    groupname = models.CharField(max_length=127)
+    teache = models.ForeingKey('Teacher')
+    students = models.ManyToManyField('studentApp.Student')
+
+class Course(models.Model):
+    title = models.TextField(max_length=127, verbose_name='Название теста')
+    description = models.TextField(max_length=255, verbose_name = 'Описание')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    tests = models.ManyToMany('authorApp.Test')
+    groups = models.ManyToMany('Group')
+    progress = models.FloatField()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title', 'time_update']
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
