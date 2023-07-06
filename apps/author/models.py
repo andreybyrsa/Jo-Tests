b81912175b19f68e08a5fe1055176bc97cca4930
cap111ffs <116576128+cap111ffs@users.noreply.github.com>
2023-07-06 15:10:06 +0500

    Delete apps/author directory

--- apps/author/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class Author(AbstractUser):
-    profile_picture = models.FileField(null=True, blank=True, max_length=500)
-    tests = models.ManyToManyField('Test')
-    role = models.CharField(default='author')
-
