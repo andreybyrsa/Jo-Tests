b13b0b627ccd8975a572c0f95b472476807c3260
TimurMA <timur.minyazeff@gmail.com>
2023-07-07 21:03:10 +0300

    включил приложение author в проект

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("auth/", include('apps.auth.urls')),
-
+    path('tests/', include('apps.author.urls')),
     path("admin/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
