15eb8abda68045dfb0493f1413cb2e5884d55cba
TimurMA <timur.minyazeff@gmail.com>
2023-07-07 21:05:46 +0300

    добавил отображение списка тестов

---
+++ apps/author/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import ViewTests, test_create
+
+urlpatterns = [
+    path("", ViewTests.as_view(), name="tests"),
+    path('create_test/', test_create, name='create_test'),
+    path('profile/', test_create, name='profile'),
+    path('change_test/', test_create, name='change-test'),
+    path('delete_test/', test_create, name='delete-test'),
+    path('dfw/', test_create, name=''),
+    path('courses/', test_create, name='inspect_course'),
+    path('profile/', test_create, name='create-course'),
+    path('tests/', test_create, name='inspect-test'),
+    path('courses/', test_create, name='courses'),
+]
--- apps/author/views.py
+++ apps/author/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from core.utils.mixins import HeaderMixin, InfoSidebarMixin
+from django.views.generic import ListView
+from .models import Test, Author
 
-# Create your views here.
+#СписокТестов(author, teacher)
+
+class ViewTests(HeaderMixin, InfoSidebarMixin,ListView):
+    model = Test
+    template_name = 'author/tests.html'
+    context_object_name = 'tests'
+      
+    def get_context_data(self, *, object_list=None, **kwargs):
+            context = super().get_context_data(**kwargs)
+            header_def = self.get_user_header()
+            sidebar_def = self.get_user_sidebar('test')
+            return dict(list(context.items()) + list(header_def.items()) + list(sidebar_def.items()))
+
+     
+    def get_querryset(self):
+        current_user = self.request.user
+        if current_user.role == 'teacher':
+                return Test.objects.all()
+        author = Author.objects.get(user=current_user)
+        return author.tests.all()
+
+def test_create(request):
+      return render(request, 'author/InfoSideBarTest.html')
