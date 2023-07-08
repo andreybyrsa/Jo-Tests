d72479ff6dba8e7d396bf926917c12076272eaf0
TimurMA <timur.minyazeff@gmail.com>
2023-07-08 16:28:45 +0300

    исправил отоброжение тестов

--- apps/Tests/views.py
+++ apps/Tests/views.py
@@ -14,25 +14,20 @@ class ViewTests(LoginRequiredMixin, HeaderMixin, InfoSidebarMixin, ListView):
     model = Test
     login_url = "/auth"
     template_name = "Tests/TestsPage.html"
+    context_object_name = 'tests'
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        json_tests = list(test.get_test_info() for test in context['tests'])
         header_def = self.get_user_header()
         sidebar_def = self.get_user_sidebar("test")
 
-        tests = self.get_queryset()
-        json_tests = list(test.get_test_info() for test in tests)
-
-        return {
-            "json_tests": json_tests,
-            "header": list(header_def["header"]),
-            "test_side_bar": list(sidebar_def["info"]),
-        }
+        return dict(list(context.items()) + list(header_def.items()) + list(sidebar_def.items()) + list({'json_tests': json_tests}.items()))
 
     def get_queryset(self):
         current_user = self.request.user
         if current_user.role == "teacher":
             return Test.objects.all()
-
         author = Author.objects.get(user=current_user)
         return author.tests.all()
 
