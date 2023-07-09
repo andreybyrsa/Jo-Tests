6d002d9b3c2809031b896b1ae87ec13acb1af9fd
kivaX9 <kirill.vlasov.05@inbox.tu>
2023-07-09 21:22:24 +0500

    Сделал страницу profile

--- apps/Profile/static/Profile/js/ProfilePage.js
+++ apps/Profile/static/Profile/js/ProfilePage.js
@@ -1,17 +1,43 @@
 addPageClassName("profile-page");
 
-let cell=document.querySelectorAll('#cell')
-let form=document.getElementById('form')
-let results=document.getElementById('results')
+const form = document.getElementById('form')
+const results = document.getElementById('results')
+const cell = document.querySelectorAll('.profile-page__cell')
+const active = 'profile-page__cell--active'
+const disabled = 'profile-page__cell--disabled'
 
-cell.forEach( cell => 
-   cell.addEventListener('click', (event) => {
-      if (event.target.innerHTML == 'Настройки профиля') {
-         form.style.display = 'flex'
-         results.style.display = 'none'
-      } 
-      if (event.target.innerHTML == 'Пройденные тесты') {
-         form.style.display = 'none'
-         results.style.display = 'flex'
-      }
-   }))
+Array.from(cell)[0].classList.add(active)
+Array.from(cell)[1].classList.add(disabled)
+
+function activeCell(id) {
+   Array.from(cell)[id].classList.add(active)
+   Array.from(cell)[id].classList.remove(disabled)
+}
+
+function disabledCell(id) {
+   Array.from(cell)[id].classList.add(disabled)
+   Array.from(cell)[id].classList.remove(active)
+}
+
+function toggleContent(id) {
+   if (id == 0) {
+      activeCell(0)
+      disabledCell(1)
+   } else {
+      activeCell(1)
+      disabledCell(0)
+   }
+}
+
+function changeContent(itemId) {
+
+   if (itemId == '0') {
+      toggleContent(itemId)
+      form.style.display = 'flex'
+      results.style.display = 'none'
+   } else {
+      toggleContent(itemId)
+      form.style.display = 'none'
+      results.style.display = 'flex'
+   }
+}
