8ffaf8e2b9854c7ff2043318b6555e000457a1d8
kivaX9 <kirill.vlasov.05@inbox.tu>
2023-07-09 15:04:47 +0500

    Сделал страницу профиля

--- apps/Profile/static/Profile/js/ProfilePage.js
+++ apps/Profile/static/Profile/js/ProfilePage.js
@@ -1,1 +1,17 @@
 addPageClassName("profile-page");
+
+let cell=document.querySelectorAll('#cell')
+let form=document.getElementById('form')
+let results=document.getElementById('results')
+
+cell.forEach( cell => 
+   cell.addEventListener('click', (event) => {
+      if (event.target.innerHTML == 'Настройки профиля') {
+         form.style.display = 'flex'
+         results.style.display = 'none'
+      } 
+      if (event.target.innerHTML == 'Пройденные тесты') {
+         form.style.display = 'none'
+         results.style.display = 'flex'
+      }
+   }))
