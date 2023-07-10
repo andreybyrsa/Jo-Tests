fbd9f228d5da8a7059c4abd5efbfc8481ebde68b
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-07-10 19:46:51 +0500

    Изменил панельку админа для просмотра юзеров

--- apps/auth/admin.py
+++ apps/auth/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-
 from .models import User, Author, Student, Teacher
 
-admin.site.register(User)
-admin.site.register(Author)
-admin.site.register(Student)
-admin.site.register(Teacher)
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("User info", {"fields": ["username", "first_name", "last_name", "password"]}),
+        ("User data", {"fields": ["profile_picture"]}),
+    ]
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    filter_horizontal = ("tests",)
+
+
+class StudentAdmin(admin.ModelAdmin):
+    filter_horizontal = ("courses", "result_tests")
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    filter_horizontal = ("courses", "groups")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Teacher, TeacherAdmin)
