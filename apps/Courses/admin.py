1e045c3bb56076aec9a0f24774a50a7aa1205da1
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-07-10 19:40:43 +0500

    Изменил панельку админа для курсов

--- apps/Courses/admin.py
+++ apps/Courses/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group as DefaultGroup
-
 from .models import Group, Course
 
 admin.site.unregister(DefaultGroup)
 
-admin.site.register(Group)
-admin.site.register(Course)
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("groupname", "teacher")
+    filter_horizontal = ("students",)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "teacher")
+    filter_horizontal = ("tests", "groups")
