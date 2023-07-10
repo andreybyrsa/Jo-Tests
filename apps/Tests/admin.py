9f8bca13c34cdc209de5e5ecbc72060356e156ff
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-07-10 19:45:47 +0500

    Изменил панельку админа для тестов

--- apps/Tests/admin.py
+++ apps/Tests/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-
 from .models import Test, StudentResult, Question, Answer, Choice
 
-admin.site.register(Test)
-admin.site.register(StudentResult)
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(Choice)
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ("title", "author")
+    filter_horizontal = ("questions",)
+
+
+@admin.register(StudentResult)
+class StudentResultAdmin(admin.ModelAdmin):
+    list_display = ("student", "test", "result")
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question", "test")
+    filter_horizontal = ("answers",)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("answer", "question")
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("student", "question", "answer", "is_selected")
