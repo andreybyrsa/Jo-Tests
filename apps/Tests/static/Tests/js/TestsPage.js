79e2c2ca4668681c3d1c6eaf31b44aa43d42cde3
andreybyrsa <byrsa2004@bk.ru>
2023-07-12 12:57:11 +0500

    Сделал поиск тестов на странице TestsPage.html

--- apps/Tests/static/Tests/js/TestsPage.js
+++ apps/Tests/static/Tests/js/TestsPage.js
@@ -6,3 +6,29 @@ const JSON_DATA = JSON.parse(dataTests);
 const tests = document.querySelectorAll(".tests-page__test");
 
 useInfoSideBar(JSON_DATA, tests, "tests-page__test");
+
+const searchInput = document.getElementById("search-input");
+const searchIcon = document.getElementById("search-icon");
+
+const ACTIVE_ICON_COLOR = "#4360F8";
+const DISABLED_ICON_COLOR = "#898989";
+
+searchInput.addEventListener("input", (event) => {
+  const searchedValue = event.target.value.toLowerCase();
+
+  if (searchedValue) {
+    searchIcon.style.color = ACTIVE_ICON_COLOR;
+  } else {
+    searchIcon.style.color = DISABLED_ICON_COLOR;
+  }
+
+  Array.from(tests).forEach((test) => {
+    const currentTestTitle = test.childNodes[1].textContent.toLowerCase();
+
+    if (currentTestTitle.includes(searchedValue)) {
+      test.style.display = "flex";
+    } else {
+      test.style.display = "none";
+    }
+  });
+});
