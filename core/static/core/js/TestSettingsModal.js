6c180a502961821403215c064bbd933634ecf267
andreybyrsa <byrsa2004@bk.ru>
2023-07-12 11:51:42 +0500

    Сделал анимацию для toggle button

--- core/static/core/js/TestSettingsModal.js
+++ core/static/core/js/TestSettingsModal.js
@@ -1,35 +1,49 @@
 const testModal = document.getElementById("test-settings-modal");
 
 const closeTestModalButton = document.getElementById("close-test-modal-button");
+
 const toggleButton = document.getElementById("toggle-button");
-
-const testSlug = document.getElementById("test-slug");
-
-const toggle = document.getElementById("toggle");
-const availableInput = document.getElementById("test-available");
+const toggleAvailable = document.getElementById("test-available");
 
 const OPENING_TEST_SETTINGS_MODAL_CLASS = "test-settings-modal--opened";
 const CLOSING_TEST_SETTINGS_MODAL_CLASS = "test-settings-modal--closed";
 
+const ACTIVE_TOGGLE_BUTTON_CLASS = "test-settings-modal__toggle-button--active";
+
 const ACTIVE_TOGGLE_CLASS = "test-settings-modal__toggle--active";
+const DISABEL_TOGGLE_CLASS = "test-settings-modal__toggle--disable";
 
 toggleButton.addEventListener("click", () => {
-  if (toggle.classList.contains(ACTIVE_TOGGLE_CLASS)) {
-    toggle.classList.remove(ACTIVE_TOGGLE_CLASS);
-    availableInput.value = false;
+  if (toggleAvailable.classList.contains(ACTIVE_TOGGLE_CLASS)) {
+    toggleButton.classList.remove(ACTIVE_TOGGLE_BUTTON_CLASS);
+    toggleAvailable.classList.add(DISABEL_TOGGLE_CLASS);
+
+    setTimeout(() => {
+      toggleAvailable.classList.remove(ACTIVE_TOGGLE_CLASS);
+      toggleAvailable.classList.remove(DISABEL_TOGGLE_CLASS);
+    }, 300);
+
+    toggleAvailable.value = false;
   } else {
-    toggle.classList.add(ACTIVE_TOGGLE_CLASS);
-    availableInput.value = true;
+    toggleButton.classList.add(ACTIVE_TOGGLE_BUTTON_CLASS);
+    toggleAvailable.classList.add(ACTIVE_TOGGLE_CLASS);
+
+    toggleAvailable.value = true;
   }
 });
 
-function openTestModal(slug) {
-  testModal.style.display = "flex";
+function openTestModal(currentTest) {
+  const testTime = document.getElementById("test-time");
 
-  testSlug.value = slug;
+  testModal.style.display = "flex";
 
   testModal.classList.add(OPENING_TEST_SETTINGS_MODAL_CLASS);
   openModal();
+
+  removeToggleClassNames();
+
+  testTime.value = currentTest?.test_time ? currentTest.test_time : 60;
+  switchToggle(currentTest?.available);
 }
 
 function closeTestModal() {
@@ -52,3 +66,15 @@ closeTestModalButton.addEventListener("click", () => {
   closeTestModal();
   closeModal();
 });
+
+function switchToggle(available) {
+  if (available == "true") {
+    toggleButton.click();
+  }
+}
+
+function removeToggleClassNames() {
+  toggleAvailable.classList.remove(ACTIVE_TOGGLE_CLASS);
+  toggleAvailable.classList.remove(DISABEL_TOGGLE_CLASS);
+  toggleButton.classList.remove(ACTIVE_TOGGLE_BUTTON_CLASS);
+}
