a0f451210a7aaca848d0b7f6f57680bb1805edb0
andreybyrsa <byrsa2004@bk.ru>
2023-07-12 20:03:24 +0500

    Сделал js файл для модального окна Profile

---
+++ core/static/core/js/ProfileModal.js
@@ -0,0 +1,28 @@
+let profileModal = document.getElementById("profile-modal");
+
+const OPENING_PROFILE_MODAL_CLASS = "profile-modal--opened";
+const CLOSING_PROFILE_MODAL_CLASS = "profile-modal--closed";
+
+if (alertModal !== null) {
+  openAlertModal();
+}
+
+function openProfileModal() {
+  profileModal.classList.add(OPENING_PROFILE_MODAL_CLASS);
+  openModal();
+}
+
+function closeProfileModal() {
+  profileModal.classList.add(CLOSING_PROFILE_MODAL_CLASS);
+
+  setTimeout(() => {
+    profileModal.classList.remove(OPENING_PROFILE_MODAL_CLASS);
+    profileModal.classList.remove(CLOSING_PROFILE_MODAL_CLASS);
+  }, 300);
+}
+
+modalLayout.addEventListener("click", (event) => {
+  if (event.target.id === "modal-layout") {
+    closeProfileModal();
+  }
+});
