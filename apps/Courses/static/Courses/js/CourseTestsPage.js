824ef23b8f66d4d18dbda9effc461cc2a7ca8328
andreybyrsa <byrsa2004@bk.ru>
2023-07-13 20:45:09 +0500

    Недоделал js группы на странице CourseTestsPage

--- apps/Courses/static/Courses/js/CourseTestsPage.js
+++ apps/Courses/static/Courses/js/CourseTestsPage.js
@@ -6,9 +6,13 @@ const dataCourseTests =
   document.getElementById("data-course-tests").textContent;
 const dataTestsResults =
   document.getElementById("data-tests-results").textContent;
+const dataGroups = document.getElementById("data-groups").textContent;
+const dataResults = document.getElementById("data-results").textContent;
 
 const JSON_COURSE_TESTS = JSON.parse(dataCourseTests);
 const JSON_TESTS_RESULTS = JSON.parse(dataTestsResults);
+const JSON_GROUPS = JSON.parse(dataGroups);
+const JSON_RESULTS = JSON.parse(dataResults);
 
 const sideBarTitle = document.getElementById("side-bar-title");
 const sideBarDescription = document.getElementById("side-bar-description");
@@ -22,13 +26,29 @@ const sidebarButton = document.getElementById("test-button");
 const sideBarImage = document.getElementById("side-bar-image");
 
 const tests = document.querySelectorAll(".course-tests-page__test");
+const groups = document.querySelectorAll(
+  ".test-results-side-bar__group-wrapper"
+);
 
 const ACTIVE_TEST_CLASS = "course-tests-page__test--active";
 const DISABLED_BUTTON_CLASS = "test-start-side-bar__button--disabled";
 
-Array.from(tests).forEach((test, index) => {
-  test.onclick = () => openSideBar(index, test);
-});
+const OPENED_GROUP_CLASS = "test-results-side-bar__group--opened";
+const CLOSED_GROUP_CLASS = "test-results-side-bar__group--closed";
+
+if (JSON_RESULTS) {
+  Array.from(tests).forEach((test, index) => {
+    test.onclick = () => openResultsSideBar(index, test);
+  });
+
+  Array.from(groups).forEach((group, index) => {
+    group.onclick = () => openGroup(index, group);
+  });
+} else {
+  Array.from(tests).forEach((test, index) => {
+    test.onclick = () => openTestSideBar(index, test);
+  });
+}
 
 function removeActiveTests() {
   Array.from(tests).forEach((test) => {
@@ -36,7 +56,7 @@ function removeActiveTests() {
   });
 }
 
-function openSideBar(index, currentTest) {
+function openTestSideBar(index, currentTest) {
   removeActiveTests();
   currentTest.classList.add(ACTIVE_TEST_CLASS);
 
@@ -62,7 +82,6 @@ function openSideBar(index, currentTest) {
     ? `${result}/${max_result}`
     : "не начато";
 
-  console.log(result);
   if (result) {
     sidebarButton.classList.add(DISABLED_BUTTON_CLASS);
     sidebarButton.textContent = "Просмотреть";
@@ -73,5 +92,37 @@ function openSideBar(index, currentTest) {
     sidebarButton.textContent = "Начать тест";
 
     sidebarButton.href = sidebarButton.getAttribute("href") + slug;
+  }
+}
+
+function openResultsSideBar(index, currentTest) {
+  removeActiveTests();
+  currentTest.classList.add(ACTIVE_TEST_CLASS);
+
+  const currentTestInfo = JSON_COURSE_TESTS[index];
+  const { test } = currentTestInfo;
+  const { title, description } = test;
+
+  sideBarImage.style.display = "none";
+  sideBarContent.style.display = "flex";
+
+  sideBarTitle.textContent = title;
+  sideBarDescription.textContent = description;
+}
+
+function openGroup(index, currentGroup) {
+  const groupContent = Array.from(currentGroup.childNodes).find(
+    (element) => element.classList?.contains("test-results-side-bar__group")
+  );
+
+  if (groupContent.classList.contains(OPENED_GROUP_CLASS)) {
+    groupContent.classList.add(CLOSED_GROUP_CLASS);
+
+    setTimeout(() => {
+      groupContent.classList.remove(OPENED_GROUP_CLASS);
+      groupContent.classList.remove(CLOSED_GROUP_CLASS);
+    }, 300);
+  } else {
+    groupContent.classList.add(OPENED_GROUP_CLASS);
   }
 }
