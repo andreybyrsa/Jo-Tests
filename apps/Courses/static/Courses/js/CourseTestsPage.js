4718d6ba6bb7a6be6d6e8b2b1a98f84b74d86d5b
andreybyrsa <byrsa2004@bk.ru>
2023-07-13 17:42:28 +0500

    Сделал страницу тестов в курсе

---
+++ apps/Courses/static/Courses/js/CourseTestsPage.js
@@ -0,0 +1,72 @@
+import { addPageClassName } from "/static/core/js/PageLayout.js";
+
+addPageClassName("course-tests-page");
+
+const dataCourseTests =
+  document.getElementById("data-course-tests").textContent;
+const dataTestsResults =
+  document.getElementById("data-tests-results").textContent;
+
+const JSON_COURSE_TESTS = JSON.parse(dataCourseTests);
+const JSON_TESTS_RESULTS = JSON.parse(dataTestsResults);
+
+const sideBarTitle = document.getElementById("side-bar-title");
+const sideBarDescription = document.getElementById("side-bar-description");
+
+const sideBarContent = document.getElementById("side-bar-content");
+const sideBarQuestionsAmount = document.getElementById("test-question-amount");
+const sideBarTestResult = document.getElementById("test-result");
+const sideBarTestTime = document.getElementById("test-time");
+const sidebarButton = document.getElementById("test-button");
+
+const sideBarImage = document.getElementById("side-bar-image");
+
+const tests = document.querySelectorAll(".course-tests-page__test");
+
+const ACTIVE_TEST_CLASS = "course-tests-page__test--active";
+
+Array.from(tests).forEach((test, index) => {
+  test.onclick = () => openSideBar(index, test);
+});
+
+function removeActiveTests() {
+  Array.from(tests).forEach((test) => {
+    test.classList.remove(ACTIVE_TEST_CLASS);
+  });
+}
+
+function openSideBar(index, currentTest) {
+  removeActiveTests();
+  currentTest.classList.add(ACTIVE_TEST_CLASS);
+
+  const currentTestInfo = JSON_COURSE_TESTS[index];
+  const { test, test_time } = currentTestInfo;
+  const { title, description, questions_amount, slug } = test;
+
+  const currentTestResult = Array.from(JSON_TESTS_RESULTS).find(
+    (test) => test.test_slug === slug
+  );
+  const { result, max_result, result_slug } = currentTestResult;
+
+  sideBarImage.style.display = "none";
+  sidebarButton.style.display = "flex";
+  sideBarContent.style.display = "flex";
+
+  sideBarTitle.textContent = title;
+  sideBarDescription.textContent = description;
+
+  sideBarQuestionsAmount.textContent = questions_amount;
+  sideBarTestTime.textContent = `${test_time} минут`;
+  sideBarTestResult.textContent = result
+    ? `${result}/${max_result}`
+    : "не начато";
+
+  if (result) {
+    sidebarButton.classList.add("test-start-side-bar__button--disabled");
+    sidebarButton.textContent = "Просмотреть";
+
+    sidebarButton.href = sidebarButton.getAttribute("href") + result_slug;
+  } else {
+    sidebarButton.href = sidebarButton.getAttribute("href") + slug;
+  }
+}
